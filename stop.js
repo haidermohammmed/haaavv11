ad51f2479fe04e18ad8070cfe93ab731e8f923ca
haidermohammmed <uuuu112277@gmail.com>
2025-11-21 23:27:37 +0300

    Add files via upload

--- stop.js
+++ stop.js
@@ -3,31 +3,23 @@ const { EmbedBuilder, Colors } = require("discord.js");
 module.exports = {
     name: "stop",
     description: "Stop the music and clear the queue.",
-    async execute(interaction, client) {
-        const { guild, user } = interaction;
-        const queue = client.distube.getQueue(guild.id);
+    async execute(message, args, client) {
+        const queue = client.distube.getQueue(message.guild.id);
         const embed = new EmbedBuilder().setColor(Colors.Blue);
 
         if (!queue) {
             embed.setDescription(client.localization.get('errors.noMusicPlaying'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
-        }
-
-        const djRole = guild.roles.cache.find(role => role.name === client.config.djRoleName);
-        const hasDJRole = djRole ? interaction.member.roles.cache.has(djRole.id) : false;
-        if (queue.initiatorId && user.id !== queue.initiatorId && !hasDJRole) {
-            embed.setDescription(client.localization.get('errors.notAuthorized'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         }
 
         try {
             queue.stop();
             embed.setDescription(client.localization.get('commands.stop.stopped'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         } catch (error) {
-            console.error("❌ Error executing stop button:", error);
+            console.error("❌ Error executing !stop command:", error);
             embed.setDescription(client.localization.get('errors.cannotStop'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         }
     },
 };
