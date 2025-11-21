c3e1f3477676ec8bec84903f297ecb52fd9c066d
haidermohammmed <uuuu112277@gmail.com>
2025-11-21 23:25:08 +0300

    Add files via upload

--- pause.js
+++ pause.js
@@ -3,36 +3,28 @@ const { EmbedBuilder, Colors } = require("discord.js");
 module.exports = {
     name: "pause",
     description: "Pause the currently playing song.",
-    async execute(interaction, client) {
-        const { guild, user } = interaction;
-        const queue = client.distube.getQueue(guild.id);
+    async execute(message, args, client) {
+        const queue = client.distube.getQueue(message.guild.id);
         const embed = new EmbedBuilder().setColor(Colors.Blue);
 
         if (!queue) {
             embed.setDescription(client.localization.get('errors.noMusicPlaying'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
-        }
-
-        const djRole = guild.roles.cache.find(role => role.name === client.config.djRoleName);
-        const hasDJRole = djRole ? interaction.member.roles.cache.has(djRole.id) : false;
-        if (queue.initiatorId && user.id !== queue.initiatorId && !hasDJRole) {
-            embed.setDescription(client.localization.get('errors.notAuthorized'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         }
 
         if (queue.paused) {
             embed.setDescription(client.localization.get('commands.pause.alreadyPaused'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         }
 
         try {
             queue.pause();
             embed.setDescription(client.localization.get('commands.pause.paused'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         } catch (error) {
-            console.error("❌ Error executing pause button:", error);
+            console.error("❌ Error executing !pause command:", error);
             embed.setDescription(client.localization.get('errors.cannotPause'));
-            return interaction.reply({ embeds: [embed], ephemeral: true });
+            return message.channel.send({ embeds: [embed] });
         }
     },
 };
